4570098cd5200d7710554795a7ae27f60efddc86
Alexsander <alexsander.zuniga@readinessit.com>
2020-02-26 11:09:34 -0300

    set congifs to use redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Menu from './components/Menu';
 
+//Redux
+import store from './store';
+import { Provider } from 'react-redux'
+
 
 export default function App() {
   return (
-    <>
-    <Menu />
-    <Container className='mt-5' >
-      <h1 className='text-center'>Tweets</h1>
-    </Container>
-    </>
+    <Provider store={store}>
+      <Menu />
+      <Container className='mt-5' >
+        <h1 className='text-center'>Tweets</h1>
+      </Container>
+    </Provider>
     
   );
 }
