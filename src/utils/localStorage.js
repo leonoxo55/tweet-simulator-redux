82c0f1a652c5d8c066a76015e193c95ceb5d4c4c
Alexsander <alexsander.zuniga@readinessit.com>
2020-02-28 09:56:21 -0300

    set tweets in local storage

---
+++ src/utils/localStorage.js
@@ -0,0 +1,10 @@
+export const getStateLocalStorage = () => {
+    const tweetsStorage = localStorage.getItem('tweets');
+    if(!tweetsStorage) return undefined
+    
+    return JSON.parse(tweetsStorage);
+}
+
+export const setStateLocalStorage = (state) => {
+    localStorage.setItem('tweets', JSON.stringify(state));
+}
